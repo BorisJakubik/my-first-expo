4dacedf6819fe79568b7f2d8aafc254d902ec7df
Boris Jakubík <jakubik@azu.sk>
2019-01-31 03:19:49 +0100

    UPDATE#Remove waste code

--- App.js
+++ App.js
@@ -1,24 +1,3 @@
-// import React from 'react';
-// import { StyleSheet, Text, View } from 'react-native';
-
-// export default class App extends React.Component {
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <Text>This is my app!</Text>
-//       </View>
-//     );
-//   }
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
 import React, { Fragment } from 'react';
 import { View } from 'react-native';
 import { Provider } from 'react-redux';
